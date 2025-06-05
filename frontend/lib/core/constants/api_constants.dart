71a9929cc8d37c14fd80b2be43a288b76dce46c7
Mauricio M. <mauromm1603@gmail.com>
2025-06-05 16:01:37 -0500

    Se cambia variable baseURL por la ruta de la api en la nube

--- frontend/lib/core/constants/api_constants.dart
+++ frontend/lib/core/constants/api_constants.dart
@@ -1,5 +1,7 @@
 class ApiConstants {
-  static const String baseUrl = "http://192.168.0.101:3000/api";
+  //static const String baseUrl = "http://192.168.0.101:3000/api"; //descomenta esto y cambia la ip por localhost si la api la abriste localmente
+  static const String baseUrl = "https://nodo-unv8.onrender.com/api"; //ruta para api en la nube en render
+
   static const String createPublicacionEndpoint = "$baseUrl/createPublicacion";
   static const String loginEndpoint = "$baseUrl/loginUsuario";
   static const String getTrabajadorByUserId = "$baseUrl/getTrabajadorByUserId";
