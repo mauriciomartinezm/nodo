1945f737189807c0bc3de19d4450ed1bbffe72a0
Mauricio M. <mauromm1603@gmail.com>
2025-06-10 09:14:01 -0500

    Se habilia url de render

--- frontend/lib/core/constants/api_constants.dart
+++ frontend/lib/core/constants/api_constants.dart
@@ -1,8 +1,8 @@
 class ApiConstants {
-  static const String baseUrl = "http://192.168.0.103:3000/api"; //descomenta esto y cambia la ip por localhost si la api la abriste localmente
+  //static const String baseUrl = "http://192.168.0.103:3000/api"; //descomenta esto y cambia la ip por localhost si la api la abriste localmente
 
   //static const String baseUrl = "http://10.0.2.2:3000/api"; //descomenta esto y cambia la ip por localhost si la api la abriste localmente
-  //static const String baseUrl = "https://nodo-unv8.onrender.com/api"; //ruta para api en la nube en render
+  static const String baseUrl = "https://nodo-unv8.onrender.com/api"; //ruta para api en la nube en render
 
   static const String createPublicacionEndpoint = "$baseUrl/createPublicacion";
   static const String loginEndpoint = "$baseUrl/loginUsuario";
