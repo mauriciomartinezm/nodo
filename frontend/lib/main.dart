a7055cf5c814ed4845ab57c3337541f5bfe3ddcc
Mauricio Martinez <mauriciomartinez0416@gmail.com>
2025-10-21 14:11:23 -0500

    refactor: rename files with *_widget

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -76,6 +76,7 @@ void main() async {
 }
 
 Future<bool> isFirstTime() async {
+  return true; // Temporalmente siempre true para pruebas
   final prefs = await SharedPreferences.getInstance();
   return !(prefs.getBool('seen_welcome') ?? false);
 }
